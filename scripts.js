539bd07b52ab647758ad4f10ad1d5d05e21c9344
Stefanguan2k <stefanguan2k@gmail.com>
2023-01-17 23:59:25 -0800

    Adjusted cover image and removed local fonts

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const work1 = document.getElementById('work-1');
+const work2 = document.getElementById('work-2');
+const work3 = document.getElementById('work-3');
+
+work1.addEventListener("click", () =>
+{
+    window.location.href = "/yopo.html"
+});
