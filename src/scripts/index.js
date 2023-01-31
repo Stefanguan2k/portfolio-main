364a75daaac385e6bab46e44f6942583cee1aea3
Stefanguan2k <stefanguan2k@gmail.com>
2023-01-30 17:57:40 -0800

    Disabled animation on resize

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,3 +12,20 @@ work2.addEventListener("click", () =>
 {
     window.location.href = "/yopo.html"
 });
+
+var timeOut;
+var delay = 100; //in ms
+
+window.addEventListener('resize', () => {
+    work1.classList.add('disable-anim');
+    work2.classList.add('disable-anim');
+    clearTimeout(timeOut);
+    timeOut = setTimeout(applyAnim, delay);
+});
+
+// Remove disable animation class
+
+function applyAnim() {
+    work1.classList.remove('disable-anim');
+    work2.classList.remove('disable-anim');
+}
