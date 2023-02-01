9cd502e4d06fff894c79c5260e8084c0c1df7ee6
Stefanguan2k <stefanguan2k@gmail.com>
2023-02-01 13:25:14 -0800

    Formatted with prettier

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,31 +1,29 @@
-const work1 = document.getElementById('work-1');
-const work2 = document.getElementById('work-2');
-const work3 = document.getElementById('work-3');
+const work1 = document.getElementById("work-1");
+const work2 = document.getElementById("work-2");
+const work3 = document.getElementById("work-3");
 
 //  Works card 1
-work1.addEventListener("click", () =>
-{
-    window.location.href = "/munch.html"
+work1.addEventListener("click", () => {
+  window.location.href = "/munch.html";
 });
 
-work2.addEventListener("click", () =>
-{
-    window.location.href = "/yopo.html"
+work2.addEventListener("click", () => {
+  window.location.href = "/yopo.html";
 });
 
 var timeOut;
 var delay = 100; //in ms
 
-window.addEventListener('resize', () => {
-    work1.classList.add('disable-anim');
-    work2.classList.add('disable-anim');
-    clearTimeout(timeOut);
-    timeOut = setTimeout(applyAnim, delay);
+window.addEventListener("resize", () => {
+  work1.classList.add("disable-anim");
+  work2.classList.add("disable-anim");
+  clearTimeout(timeOut);
+  timeOut = setTimeout(applyAnim, delay);
 });
 
 // Remove disable animation class
 
 function applyAnim() {
-    work1.classList.remove('disable-anim');
-    work2.classList.remove('disable-anim');
+  work1.classList.remove("disable-anim");
+  work2.classList.remove("disable-anim");
 }
--- src/scripts/munch.js
+++ src/scripts/munch.js
@@ -7,9 +7,8 @@ const cardIndicator2 = document.querySelector(".card-ct2");
 // Parallax hero text
 
 window.addEventListener("scroll", () => {
-    const dist = window.scrollY;
-    heroText.style.transform = 
-    `translateY(${dist * -0.5}px)`;
+  const dist = window.scrollY;
+  heroText.style.transform = `translateY(${dist * -0.5}px)`;
 });
 
 // Persona carousel
@@ -18,18 +17,18 @@ document.onload = togglePersonaCarousel();
 window.addEventListener("resize", togglePersonaCarousel);
 
 function togglePersonaCarousel() {
-    if (window.innerWidth <= 768) {
-        personaCards.forEach((personaCard, indx) => {
-            personaCard.style.transform = `translateX(${indx * personaGap}%)`;
-            cardIndicator1.classList.add('current-cnt');
-            cardIndicator2.classList.remove('current-cnt');
-        });
-    } else { 
-        personaCards.forEach((personaCard) => {
-            personaCard.style.removeProperty('transform');
-        })
-    }
-};
+  if (window.innerWidth <= 768) {
+    personaCards.forEach((personaCard, indx) => {
+      personaCard.style.transform = `translateX(${indx * personaGap}%)`;
+      cardIndicator1.classList.add("current-cnt");
+      cardIndicator2.classList.remove("current-cnt");
+    });
+  } else {
+    personaCards.forEach((personaCard) => {
+      personaCard.style.removeProperty("transform");
+    });
+  }
+}
 
 // Current card
 let curCard = 0;
@@ -41,52 +40,56 @@ const prevBtn = document.querySelector(".prev-btn");
 
 // Next button functionality
 nextBtn.addEventListener("click", function () {
-    if (curCard === maxCard) {
-        curCard = 0;
-        cardIndicator1.classList.add('current-cnt');
-        cardIndicator2.classList.remove('current-cnt');
-    } else {
-        curCard++;
-        cardIndicator1.classList.remove('current-cnt');
-        cardIndicator2.classList.add('current-cnt');
-    }
-    personaCards.forEach((personaCard, index) => {
-        personaCard.style.transform = `translateX(${personaGap * (index - curCard)}%)`;
-    });
+  if (curCard === maxCard) {
+    curCard = 0;
+    cardIndicator1.classList.add("current-cnt");
+    cardIndicator2.classList.remove("current-cnt");
+  } else {
+    curCard++;
+    cardIndicator1.classList.remove("current-cnt");
+    cardIndicator2.classList.add("current-cnt");
+  }
+  personaCards.forEach((personaCard, index) => {
+    personaCard.style.transform = `translateX(${
+      personaGap * (index - curCard)
+    }%)`;
+  });
 });
 
 // Back button functionality
 prevBtn.addEventListener("click", function () {
-    if (curCard === 0) {
-        curCard = maxCard;
-        cardIndicator1.classList.remove('current-cnt');
-        cardIndicator2.classList.add('current-cnt');
-    } else {
-        curCard--;
-        cardIndicator1.classList.add('current-cnt');
-        cardIndicator2.classList.remove('current-cnt');
-    }
-    personaCards.forEach((personaCard, index) => {
-        personaCard.style.transform = `translateX(${personaGap * (index - curCard)}%)`;
-    });
-})
+  if (curCard === 0) {
+    curCard = maxCard;
+    cardIndicator1.classList.remove("current-cnt");
+    cardIndicator2.classList.add("current-cnt");
+  } else {
+    curCard--;
+    cardIndicator1.classList.add("current-cnt");
+    cardIndicator2.classList.remove("current-cnt");
+  }
+  personaCards.forEach((personaCard, index) => {
+    personaCard.style.transform = `translateX(${
+      personaGap * (index - curCard)
+    }%)`;
+  });
+});
 
 // Figma prototype
 const figmaBtn = document.querySelector(".figma-btn");
 const figmaOverlay = document.querySelector(".figma-overlay");
-const figma = document.querySelector('.figma');
-const closeFigmaBtn = document.querySelector('.close-figma')
+const figma = document.querySelector(".figma");
+const closeFigmaBtn = document.querySelector(".close-figma");
 
-figmaBtn.addEventListener('click', () => {
-    figmaOverlay.setAttribute('data-active', true);
-    setTimeout(() => {
-        figmaOverlay.style.opacity = 1
-    }, 200);
+figmaBtn.addEventListener("click", () => {
+  figmaOverlay.setAttribute("data-active", true);
+  setTimeout(() => {
+    figmaOverlay.style.opacity = 1;
+  }, 200);
 });
 
-closeFigmaBtn.addEventListener('click', () => {
-    figmaOverlay.style.opacity = 0
-    setTimeout(() => {
-        figmaOverlay.setAttribute('data-active', false)
-    }, 500);
+closeFigmaBtn.addEventListener("click", () => {
+  figmaOverlay.style.opacity = 0;
+  setTimeout(() => {
+    figmaOverlay.setAttribute("data-active", false);
+  }, 500);
 });
