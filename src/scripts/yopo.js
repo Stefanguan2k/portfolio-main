f3ea06e9122cbc685ccf6b9724d7103ed8359a45
Stefanguan2k <stefanguan2k@gmail.com>
2023-02-15 17:25:48 -0800

    Started projects.html

--- src/scripts/yopo.js
+++ src/scripts/yopo.js
@@ -1,8 +1,64 @@
-const heroText = document.querySelector(".title");
+const heroText = document.querySelector('.title');
+const fauxBrowser = document.querySelector('.faux-browser-container');
+const browserImg = document.querySelector('.browser-content');
+const desktopImg = document.querySelector('.desktop-img');
+const mobileImg = document.querySelector('.mobile-img');
+
+const desktopImgArray = [
+  'src/assets/img/yopo/yopoDesktopMain.png',
+  'src/assets/img/yopo/desktopMenu.png',
+  'src/assets/img/yopo/desktopOrder.png',
+  'src/assets/img/yopo/desktopContact.png',
+  'src/assets/img/yopo/yopoDesktopMain.png',
+];
+
+const desktopAltText = [
+  'The Yopo Sweet Mandarin desktop home page',
+  'The Yopo Sweet Mandarin desktop menu page',
+  'The Yopo Sweet Mandarin desktop order page',
+  'The Yopo Sweet Mandarin desktop contact page',
+  'The Yopo Sweet Mandarin desktop home page',
+];
+
+const mobileImgArray = [
+  'src/assets/img/yopo/yopoMobileMain.png',
+  'src/assets/img/yopo/mobileMenu.png',
+  'src/assets/img/yopo/mobileOrder.png',
+  'src/assets/img/yopo/mobileContact.png',
+  'src/assets/img/yopo/yopoMobileMain.png',
+];
+
+const mobileAltText = [
+  'The Yopo Sweet Mandarin mobile home page',
+  'The Yopo Sweet Mandarin mobile menu page',
+  'The Yopo Sweet Mandarin mobile order page',
+  'The Yopo Sweet Mandarin mobile contact page',
+  'The Yopo Sweet Mandarin mobile home page',
+];
 
 // Parallax hero logo
 
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
   const dist = window.scrollY;
-  heroText.style.transform = `translateY(${dist * -0.5}px)`;
+  if (dist <= window.innerHeight) {
+    heroText.style.transform = `translateY(${dist * -0.5}px)`;
+  }
+});
+
+// Determine whether to show mobile or desktop layout for final design
+var curImg = 0;
+
+//Click to change photos
+
+fauxBrowser.addEventListener('click', () => {
+  if (curImg < 4) {
+    curImg = curImg + 1;
+    desktopImg.src = desktopImgArray[curImg];
+    desktopImg.alt = desktopAltText[curImg];
+    mobileImg.src = mobileImgArray[curImg];
+    mobileImg.alt = mobileAltText[curImg];
+  }
+  if (curImg === 4) {
+    curImg = 0;
+  }
 });
