e1a6b17f58347b49bc18323c8b98e52bfc03ecd9
Stefanguan2k <stefanguan2k@gmail.com>
2023-04-18 19:32:25 -0700

    Created new temporary projects UI
    Project nav button now temporarily opens up a modal
    
    Projects page WIP (svelte rework)
    
    Made the logo icon an anchor instead of a button

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -7,6 +7,11 @@ const footerLine = document.querySelector('.footer-container');
 const linkedinBtn = document.querySelector('.linkedin-btn');
 const githubBtn = document.querySelector('.github-btn');
 const navClose = document.querySelector('.nav-close');
+const projModal = document.querySelector('.project-modal');
+const projCloseBtn = document.querySelector('.close-modal');
+const projNavBtn = document.getElementById('project-btn');
+const desktopProjBtn = document.getElementById('d-project-btn');
+const triangle = document.querySelector('.triangle');
 
 // Disable then reenable animations on load
 // to prevent unexpected animations
@@ -14,32 +19,13 @@ const navClose = document.querySelector('.nav-close');
 window.onload = () => {
   navbg.classList.add('disable-anim');
   navItems.classList.add('disable-anim');
+  projModal.classList.add('.disable-anim');
   setTimeout(() => {
     navbg.classList.remove('disable-anim');
     navItems.classList.remove('disable-anim');
+    projModal.classList.remove('.disable-anim');
   }, 500);
 };
-
-// To be implemented in the future
-// const toggleDarkMode = document.querySelector('.toggle-dark');
-// const darkModeIcon = document.getElementById('dark-mode-icon');
-
-// Toggle darkmode
-// toggleDarkMode.addEventListener('click', () => {
-//   if (toggleDarkMode.getAttribute('data-dark') === 'false') {
-//     toggleDarkMode.setAttribute('data-dark', true);
-//     darkModeIcon.setAttribute(
-//       'd',
-//       'M256 159.1c-53.02 0-95.1 42.98-95.1 95.1S202.1 351.1 256 351.1s95.1-42.98 95.1-95.1S309 159.1 256 159.1zM509.3 347L446.1 255.1l63.15-91.01c6.332-9.125 1.104-21.74-9.826-23.72l-109-19.7l-19.7-109c-1.975-10.93-14.59-16.16-23.72-9.824L256 65.89L164.1 2.736c-9.125-6.332-21.74-1.107-23.72 9.824L121.6 121.6L12.56 141.3C1.633 143.2-3.596 155.9 2.736 164.1L65.89 256l-63.15 91.01c-6.332 9.125-1.105 21.74 9.824 23.72l109 19.7l19.7 109c1.975 10.93 14.59 16.16 23.72 9.824L256 446.1l91.01 63.15c9.127 6.334 21.75 1.107 23.72-9.822l19.7-109l109-19.7C510.4 368.8 515.6 356.1 509.3 347zM256 383.1c-70.69 0-127.1-57.31-127.1-127.1c0-70.69 57.31-127.1 127.1-127.1s127.1 57.3 127.1 127.1C383.1 326.7 326.7 383.1 256 383.1z'
-//     );
-//   } else {
-//     toggleDarkMode.setAttribute('data-dark', false);
-//     darkModeIcon.setAttribute(
-//       'd',
-//       'M32 256c0-123.8 100.3-224 223.8-224c11.36 0 29.7 1.668 40.9 3.746c9.616 1.777 11.75 14.63 3.279 19.44C245 86.5 211.2 144.6 211.2 207.8c0 109.7 99.71 193 208.3 172.3c9.561-1.805 16.28 9.324 10.11 16.95C387.9 448.6 324.8 480 255.8 480C132.1 480 32 379.6 32 256z'
-//     );
-//   }
-// });
 
 //
 // Listen for hover
@@ -78,16 +64,33 @@ hamburger.addEventListener('click', () => {
   }
 });
 
+projCloseBtn.addEventListener('click', () => {
+  projModal.setAttribute('data-active', false);
+  triangle.classList.remove('r180');
+});
+
+projNavBtn.addEventListener('click', () => {
+  navbg.setAttribute('data-active', false);
+  navItems.setAttribute('data-active', false);
+  navClose.setAttribute('data-active', false);
+  projModal.setAttribute('data-active', true);
+});
+
+desktopProjBtn.addEventListener('click', () => {
+  if (projModal.getAttribute('data-active') === 'false') {
+    projModal.setAttribute('data-active', true);
+    triangle.classList.add('r180');
+  } else {
+    projModal.setAttribute('data-active', false);
+    triangle.classList.remove('r180');
+  }
+});
+
 // Click off of navbar to close
 navClose.addEventListener('click', () => {
   navbg.setAttribute('data-active', false);
   navItems.setAttribute('data-active', false);
   navClose.setAttribute('data-active', false);
-});
-
-// Logo button
-homeBtn.addEventListener('click', () => {
-  window.location.href = '/index.html';
 });
 
 // Resize navbar height and close mobile nav menu
@@ -97,6 +100,16 @@ window.addEventListener('resize', () => {
     navItems.setAttribute('data-active', false);
     navClose.setAttribute('data-active', false);
   }
+  else {
+    projModal.setAttribute('data-active', false);
+    triangle.classList.remove('r180');
+  }
+  projModal.classList.add('disable-anim');
+  setTimeout(() => {
+    navbg.classList.remove('disable-anim');
+    navItems.classList.remove('disable-anim');
+    projModal.classList.remove('disable-anim');
+  }, 500);
 });
 
 console.log('hello world!');
